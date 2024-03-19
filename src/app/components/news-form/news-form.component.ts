1a3f458628bc1c39a73ac30a39be4844af46dc30
Jonntz <monteirojonatas23@gmail.com>
2024-03-18 21:00:10 -0300

    feat: add a Form style

--- src/app/components/news-form/news-form.component.ts
+++ src/app/components/news-form/news-form.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-news-form',
   standalone: true,
-  imports: [],
+  imports: [BtnPrimaryComponent],
   templateUrl: './news-form.component.html',
   styleUrl: './news-form.component.scss'
 })
