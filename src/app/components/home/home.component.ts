11d6b4db84d1952cf0ff42e798adefc210c94499
Jonntz <monteirojonatas23@gmail.com>
2024-03-19 13:19:07 -0300

    Feat: add footer and finish styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,19 @@ import { NgOptimizedImage } from '@angular/common';
 import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 import { NewsFormComponent } from '../news-form/news-form.component';
 import { BenefitCardComponent } from '../benefit-card/benefit-card.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent, NewsFormComponent, BenefitCardComponent],
+  imports: [
+    HeaderComponent, 
+    NgOptimizedImage, 
+    BtnPrimaryComponent, 
+    NewsFormComponent, 
+    BenefitCardComponent,
+    FooterComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
